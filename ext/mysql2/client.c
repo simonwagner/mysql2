60d1fd7b9b1063d7c7e32b5a68c02d5332ca5495
Simon Wagner <swg@pomfort.com>
2021-03-24 20:34:09 +0100

    Replace occurences of my_bool

--- ext/mysql2/client.c
+++ ext/mysql2/client.c
@@ -436,7 +436,7 @@ static VALUE do_send_query(void *args) {
  */
 static void *nogvl_read_query_result(void *ptr) {
   MYSQL * client = ptr;
-  my_bool res = mysql_read_query_result(client);
+  int res = mysql_read_query_result(client);
 
   return (void *)(res == 0 ? Qtrue : Qfalse);
 }
@@ -759,7 +759,7 @@ static VALUE _mysql_client_options(VALUE self, int opt, VALUE value) {
   const void *retval = NULL;
   unsigned int intval = 0;
   const char * charval = NULL;
-  my_bool boolval;
+  bool boolval;
 
   GET_CLIENT(self);
 
