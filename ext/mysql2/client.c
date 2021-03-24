526b8ced03642e460b97caafad5a7cbdcda3cad4
Simon Wagner <swg@pomfort.com>
2021-03-24 20:49:14 +0100

    Remove usage of MYSQL_SECURE_AUTH

--- ext/mysql2/client.c
+++ ext/mysql2/client.c
@@ -794,11 +794,6 @@ static VALUE _mysql_client_options(VALUE self, int opt, VALUE value) {
       retval = &boolval;
       break;
 
-    case MYSQL_SECURE_AUTH:
-      boolval = (value == Qfalse ? 0 : 1);
-      retval = &boolval;
-      break;
-
     case MYSQL_READ_DEFAULT_FILE:
       charval = (const char *)StringValueCStr(value);
       retval  = charval;
@@ -1182,7 +1177,7 @@ static VALUE set_ssl_options(VALUE self, VALUE key, VALUE cert, VALUE ca, VALUE 
 }
 
 static VALUE set_secure_auth(VALUE self, VALUE value) {
-  return _mysql_client_options(self, MYSQL_SECURE_AUTH, value);
+  return Qfalse;
 }
 
 static VALUE set_read_default_file(VALUE self, VALUE value) {
